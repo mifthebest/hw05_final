c6105bacecadccd25c21619d09a7a04485c3f3a7
Игорь Луковец <igor.lukavets@gmail.com>
2021-10-10 10:17:27 +0300

    2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -138,8 +138,7 @@ def follow_index(request):
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
     if (
-            request.user != author and
-            not Follow.objects.filter(
+            request.user != author and not Follow.objects.filter(
                 user=request.user,
                 author=author
             ).exists()
